b0f4ea48e77a2f2380f9f5eaa0c1ca84a82c276f
Flo <florentingandelin69@gmail.com>
2026-05-06 11:55:47 +0200

    commit

--- condition.js
+++ condition.js
@@ -133,17 +133,88 @@ console.log(ticket)
 
 //EX01 Feu
 
-function FeuTricolor (){
+
+function FeuTricolor() {
     let ReponseUtilisateur = prompt("Quel couleur est le feu ?")
-    if(ReponseUtilisateur == "rouge"){
+    if (ReponseUtilisateur == "rouge") {
         console.log("Je m'arrête")
-    }else if(ReponseUtilisateur == "orange"){
+    } else if (ReponseUtilisateur == "orange") {
         console.log("Je freine ! ")
-    }else if(ReponseUtilisateur == "vert"){
+    } else if (ReponseUtilisateur == "vert") {
         console.log("Je Passe !")
 
-    }else{
+    } else {
         console.log("Danger : feu défectueux")
     }
 }
 
+
+
+//calculer une moyenne genrale 
+//role : calculer la moyenen G de l'eleve
+//paramtre : les moyennes des différentes matiéres (4)
+//return : la moyenne genrale
+function calculerMoyenne(noteMath, noteFrancais, noteHistoire, noteSVT){
+
+    let moyenneG = (noteFrancais+noteHistoire+noteMath+noteSVT)/4
+    return moyenneG
+}
+
+//créer un commentaire 
+//role : créer un commentaire en focntion de la moyenne Generale
+//parametre : la moyenne G de l'eleve
+//return : le commentaire 
+
+function genererCommentaire(moyenneGenerale){
+
+    //si la moyenen generale est inférieur ou égale a 10 -> travailler plus 
+    if(moyenneGenerale <=10){
+        return "Travaillez plus"
+    }
+
+    //sinon si la moyenen genrale est entre 10 et 13 -> poursuivez effort
+    else if(moyenneGenerale>10 && moyenneGenerale<=13){
+        return "poursuivez vos efforts"
+    }
+
+    //sinon si lamoyenne g est entre 13 et 15 -> bon travail 
+    else if (moyenneGenerale>13 && moyenneGenerale<=15){
+        return "bon travail"
+    }
+
+    //sinon si la moyenen g est entre 15 et 17 -> bravo 
+    else if(moyenneGenerale>15 && moyenneGenerale<=17){
+        return "bravo"
+    }
+
+    //sinon -> felicitation
+    else{
+        return "felicitations"
+    }
+
+}
+
+//éditer bulletin
+//role : Afficher le bulletin 
+//parametre : la moyenen G et le commentaire 
+//return : rien car elle affiche 
+
+function afficherBulletin(commentaire, moyenneGenerale, prenomEleve){
+
+    alert(`La moyenne de l'éléve ${prenomEleve} est : ${moyenneGenerale} et le commentaire est : ${commentaire}`)
+
+}
+
+//role : jouer le scenario -> calculermoyenne, commentaire, buleetin
+//paramtre : notes
+//return : rien
+
+function scenario(a,b,c,d,prenomEleve){
+    let moyenneG = calculerMoyenne(a, b, c, d)
+    let commentaire = genererCommentaire(moyenneG)
+    afficherBulletin(commentaire, moyenneG, prenomEleve)
+}
+
+scenario(9,8,7,3,"Marie");
+scenario(9,8,7,3,"Paul");
+scenario(9,8,7,3,"Jean");
