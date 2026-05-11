db98a532dac4af737aeb9ebd7f238829faf1e0b6
Flo <florentingandelin69@gmail.com>
2026-05-11 09:53:12 +0200

    commit

---
+++ boucle.js
@@ -0,0 +1,17 @@
+/* for(let i=100; i>0; i=i-2){
+    console.log (i)
+}
+*/
+
+//role: afficher la table de multipiclation d'un nombre quelconque
+//parametre: nombre quelconque
+//return :
+
+function TableMultiplication(chiffreAMultiplier){
+    for(let i=1; i<=10; i++){
+        console.log(`${chiffreAMultiplier} x ${i} = ${chiffreAMultiplier*i}`)
+    }
+}
+
+
+
