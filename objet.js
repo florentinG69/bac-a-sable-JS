5e17fc71f99f901161dd3b412741fea715068214
Flo <florentingandelin69@gmail.com>
2026-05-12 14:06:09 +0200

    commit

--- objet.js
+++ objet.js
@@ -1,3 +1,5 @@
+
+/*
 let promo11 = [
 
     {
@@ -57,6 +59,96 @@ function ajouterAuBody(trucAafficher) {
 }
 
 ficheEleveDansDOM(promo11)
+
+*/
+
+let recette = {
+    nom: "poulet rôti au thym et à l'ail",
+    difficulte: "Facile",
+    tempPreparation: "15 min",
+    tempCuisson: "1h 15 min",
+    nbrPortions: 6,
+    ingredients: ["huile d'olive", "beurre fondu", "feuilles de thym", "sel", "poivre", "ail"]
+}
+
+console.log(`
+    La recette du ${recette.nom}
+    Difficulté : ${recette.difficulte}
+    Temps de préparation : ${recette.tempPreparation}
+    Temps de cuisson : ${recette.tempCuisson}
+    Pour ${recette.nbrPortions}
+    Liste des ingrédients : ${recette.ingredients}
+`)
+
+
+let recettes = [
+    {
+        nom: "poulet rôti au thym et à l'ail",
+        difficulte: "Facile",
+        tempPreparation: "15 min",
+        tempCuisson: "1h 15 min",
+        nbrPortions: 6,
+        ingredients: ["huile d'olive", "beurre fondu", "feuilles de thym", "sel", "poivre", "ail"]
+    },
+    {
+        nom: "spaghetti à la bolognaise",
+        difficulte: "Moyenne",
+        tempPreparation: "20 min",
+        tempCuisson: "40 min",
+        nbrPortions: 4,
+        ingredients: ["spaghetti", "viande hachée", "oignon", "carotte", "tomate concassée", "ail", "huile d'olive", "sel", "poivre"]
+    },
+    {
+        nom: "salade César",
+        difficulte: "Facile",
+        tempPreparation: "15 min",
+        tempCuisson: "0 min",
+        nbrPortions: 2,
+        ingredients: ["laitue romaine", "croûtons", "parmesan râpé", "filet de poulet", "sauce César", "sel", "poivre"]
+    }
+];
+
+//role : récuperer les recettes une par une pour les afficher
+//parametre : tableau des recette
+//return : rien
+function ficheRecette(recettes) {
+    //recuperer des recette une par une
+
+    recettes.forEach(recette => {
+        recette.ingredients.forEach(ingredient => {
+
+            ingredientsListe += `<li> ${ingredient}</li>`
+            console.log(ingredientsListe)
+        })
+
+        //quand il en tiens une il l'affiche
+        ajouterAuBody(`
+            <div>
+                <h2>La recette du ${recette.nom}</h2>
+                <p>Difficulté : ${recette.difficulte}</p>
+                <p>Temps de préparation : ${recette.tempPreparation}</p>
+                <p>Temps de cuisson : ${recette.tempCuisson}</p>
+                <p>Pour ${recette.nbrPortions}</p>
+                <p>Liste des ingrédients :</p> 
+                <ul>
+                ${recette.ingredients}
+                </ul>  
+            </div>
+
+            `)
+
+    })
+
+};
+
+
+
+function ajouterAuBody(trucAafficher) {
+    document.querySelector("body").innerHTML += trucAafficher
+
+}
+
+
 
 
 
